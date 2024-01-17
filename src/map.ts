811c833469ca032c88792b222b60f3ac406c4739
okou <29059223+ookkoouu@users.noreply.github.com>
2024-01-18 02:09:42 +0900

    fix(map): add type argument of key

--- src/map.ts
+++ src/map.ts
@@ -1,33 +1,30 @@
 import { Storage, type StorageOptions } from "./storage";
 
-export type MapStorageOptions<T> = {
-  defaultValue: Record<string, T> | Array<[string, T]>;
+export type MapStorageOptions<K extends string, V> = {
+  defaultValue: Record<K, V> | Array<[K, V]>;
 } & StorageOptions;
 
-export class MapStorage<T> implements Map<string, T> {
-  protected storage: Storage<Array<[string, T]>>;
-  protected cache: Map<string, T>;
-  readonly defaultValue: Array<[string, T]>;
+export class MapStorage<K extends string, V> implements Map<K, V> {
+  protected storage: Storage<Array<[K, V]>>;
+  protected cache: Map<K, V>;
+  readonly defaultValue: Array<[K, V]>;
   get size(): number {
     return this.cache.size;
   }
 
   onChanged: Pick<
     chrome.events.Event<
-      (change: {
-        newValue?: Array<[string, T]>;
-        oldValue?: Array<[string, T]>;
-      }) => void
+      (change: { newValue?: Array<[K, V]>; oldValue?: Array<[K, V]> }) => void
     >,
     "addListener" | "hasListener" | "removeListener"
   >;
 
-  constructor(key: string, options?: Partial<MapStorageOptions<T>>) {
+  constructor(key: string, options?: Partial<MapStorageOptions<K, V>>) {
     let init = options?.defaultValue ?? [];
-    init = Array.isArray(init) ? init : Object.entries(init);
+    init = Array.isArray(init) ? init : (Object.entries(init) as [K, V][]);
     this.defaultValue = init;
     this.storage = new Storage(key, init, options);
-    this.cache = new Map<string, T>();
+    this.cache = new Map<K, V>();
     this.onChanged = this.storage.onChanged;
   }
 
@@ -37,7 +34,7 @@ export class MapStorage<T> implements Map<string, T> {
         return;
       }
 
-      this.cache = new Map(change.newValue);
+      this.cache = new Map<K, V>(change.newValue);
     });
   }
 
@@ -50,54 +47,54 @@ export class MapStorage<T> implements Map<string, T> {
     this.cache = new Map(remote);
   }
 
-  toObject(): Record<string, T> {
-    return Object.fromEntries(this.cache);
+  toObject(): Record<K, V> {
+    return Object.fromEntries(this.cache.entries()) as Record<K, V>;
   }
 
   clear(): void {
     this.cache.clear();
   }
 
-  delete(key: string): boolean {
+  delete(key: K): boolean {
     const res = this.cache.delete(key);
     this.save();
     return res;
   }
 
   forEach(
-    callbackfn: (value: T, key: string, map: Map<string, T>) => void,
+    callbackfn: (value: V, key: K, map: Map<K, V>) => void,
     thisArg?: unknown,
   ): void {
     this.cache.forEach(callbackfn, thisArg);
   }
 
-  get(key: string): T | undefined {
+  get(key: K): V | undefined {
     return this.cache.get(key);
   }
 
-  has(key: string): boolean {
+  has(key: K): boolean {
     return this.cache.has(key);
   }
 
-  set(key: string, value: T): this {
+  set(key: K, value: V): this {
     this.cache.set(key, value);
     this.save();
     return this;
   }
 
-  entries(): IterableIterator<[string, T]> {
+  entries(): IterableIterator<[K, V]> {
     return this.cache.entries();
   }
 
-  keys(): IterableIterator<string> {
+  keys(): IterableIterator<K> {
     return this.cache.keys();
   }
 
-  values(): IterableIterator<T> {
+  values(): IterableIterator<V> {
     return this.cache.values();
   }
 
-  [Symbol.iterator](): IterableIterator<[string, T]> {
+  [Symbol.iterator](): IterableIterator<[K, V]> {
     return this.cache.entries();
   }
 
